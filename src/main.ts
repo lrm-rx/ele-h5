a295031e75a1758124c5fe1518f7dba2b32d9fa4
Ming <756131502@qq.com>
2024-09-23 22:22:16 +0800

    chore: 配置项目目录结构

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
