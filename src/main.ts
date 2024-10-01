51666f443850c8242b316ec9d5b9d10b52d4d31f
Ming <756131502@qq.com>
2024-10-01 19:54:05 +0800

    feat: scrollBar组件

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'vant/lib/index.css'
+import './assets/common.scss'
 
 const app = createApp(App)
 
--- src/views/tabs/home/HomeView.vue
+++ src/views/tabs/home/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import type { IHomeInfo } from '@/types'
+import type { ICountdown, IHomeInfo } from '@/types'
 import { fetchHomePageData } from '@/api/home'
+import OpLoadingView from '@/components/OpLoadingView.vue'
 import { useAsync } from '@/use/useAsync'
 import { useToggle } from '@/use/useToggle'
 import SearchView from '@/views/search/SearchView.vue'
+import ScrollBar from './components/ScrollBar.vue'
 import TheTop from './components/TheTop.vue'
+import TheTransformer from './components/TheTransformer.vue'
 
 const recomments = [
   {
@@ -18,7 +21,14 @@ const recomments = [
 ]
 
 const [isSearchViewShow, toggleSearchView] = useToggle(false)
-const { data, pending } = useAsync(fetchHomePageData, {} as IHomeInfo)
+const { data, pending } = useAsync(fetchHomePageData, {
+  banner: [],
+  searchRecomments: [],
+  transformer: [],
+  scrollBarInfoList: [],
+  countdown: {} as ICountdown,
+  activities: [],
+} as IHomeInfo)
 </script>
 
 <template>
@@ -30,9 +40,14 @@ const { data, pending } = useAsync(fetchHomePageData, {} as IHomeInfo)
 
     <!-- 只做展示 -->
     <TheTop :recomments @search-click="toggleSearchView" />
-
-    {{ data }}
-    {{ pending }}
+    <OpLoadingView :loading="pending" type="skeleton">
+      <div class="home-page__banner">
+        <img v-for="v in data.banner" :key="v.imgUrl" :src="v.imgUrl">
+      </div>
+      <TheTransformer :data="data.transformer" />
+    </OpLoadingView>
+    <!-- 需要加上v-if -->
+    <ScrollBar v-if="data.scrollBarInfoList.length" :data="data.scrollBarInfoList" />
   </div>
 </template>
 
@@ -45,5 +60,36 @@ const { data, pending } = useAsync(fetchHomePageData, {} as IHomeInfo)
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+
+.home-page {
+  padding-bottom: 70px;
+  background: var(--op-gray-bg-color);
+
+  &__banner {
+    img {
+      width: 100%;
+      padding-top: 10px;
+      background: white;
+    }
+  }
+
+  &__activity {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 10px;
+
+    &__swipe {
+      width: 180px;
+      height: 170px;
+      border-radius: 8px;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
 }
 </style>
